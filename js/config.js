65125878cf021b920e310f2be43c472ca4b96ee6
Socrates44 <124853332+Socrates44@users.noreply.github.com>
2026-07-27 17:38:17 -0600

    Update Supabase URL and publishable key

--- js/config.js
+++ js/config.js
@@ -1,10 +1,8 @@
 "use strict";
 
-const SUPABASE_URL =
-  "https://fztasvfkvdqjwajhcbfe.supabase.co";
+const SUPABASE_URL = "https://fztaswfvxkqjwajhcbfe.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY =
-  "sb_publishable_rszD07mbsSkmmp3vAvIjVQ_Hij2_1gU";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_rszD07mbsSkmmp3vAvIjVQ_Hij2_1gU";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
